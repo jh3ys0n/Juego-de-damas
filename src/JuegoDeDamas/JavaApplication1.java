08c62de10c0335c4d114931d126ea1f6854ae5dd
jh3ys0n <gazelaldena@gmail.com>
2022-06-01 06:39:18 -0400

    Metodo para repintar el tablero

--- src/JuegoDeDamas/JavaApplication1.java
+++ src/JuegoDeDamas/JavaApplication1.java
@@ -4,9 +4,10 @@
  */
 package JuegoDeDamas;
 
-import JuegoDeDamas.Agentes.JugadorNegro;
 import JuegoDeDamas.Juego.Tablero;
 import java.io.IOException;
+import java.util.ArrayList;
+import javax.swing.JFrame;
 
 public class JavaApplication1 {
 
@@ -15,10 +16,74 @@ public class JavaApplication1 {
      * @throws java.io.IOException
      */
    public static void main(String[] args) throws IOException {
-       Tablero tablero= new Tablero();
-       //System.out.println(tablero.getMatrix().toString());
-//tablero.setTablero();
-      
+       
+       Tablero tablero=new Tablero();
+       
+        JFrame frame = new JFrame();
+        frame.add(tablero);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationByPlatform(true);
+        frame.pack();
+        frame.setVisible(true);
+       
+        
+        
+       // tablero.setLista(matrix2);
+        
     }
     
 }  
+
+
+/*
+    ArrayList<ArrayList<String>> matrix=new ArrayList<ArrayList<String>>();
+    
+        ArrayList<String>fila1=new ArrayList<String>();
+        fila1.add("0");fila1.add("1");fila1.add("0");fila1.add("1");fila1.add("0");fila1.add("1");fila1.add("0");fila1.add("1");
+
+        ArrayList<String>fila2=new ArrayList<String>();
+        fila2.add("1");fila2.add("0");fila2.add("1");fila2.add("0");fila2.add("1");fila2.add("0");fila2.add("1");fila2.add("0");
+
+        ArrayList<String>filaCP=new ArrayList<String>();
+        filaCP.add("B");filaCP.add("1");filaCP.add("B");filaCP.add("1");filaCP.add("B");filaCP.add("1");filaCP.add("B");filaCP.add("1");
+
+        ArrayList<String>filaCP2=new ArrayList<String>();
+        filaCP2.add("1");filaCP2.add("B");filaCP2.add("1");filaCP2.add("B");filaCP2.add("1");filaCP2.add("B");filaCP2.add("1");filaCP2.add("B");
+
+        ArrayList<String>filaCPN=new ArrayList<String>();
+        filaCPN.add("N");filaCPN.add("1");filaCPN.add("N");filaCPN.add("1");filaCPN.add("N");filaCPN.add("1");filaCPN.add("N");filaCPN.add("1");
+
+        ArrayList<String>filaCPN2=new ArrayList<String>();
+        filaCPN2.add("1");filaCPN2.add("N");filaCPN2.add("1");filaCPN2.add("N");filaCPN2.add("1");filaCPN2.add("N");filaCPN2.add("1");filaCPN2.add("N");
+
+        matrix.add(fila1);
+        matrix.add(filaCP2);
+        matrix.add(fila1);
+        matrix.add(fila2);
+        matrix.add(fila1);
+        matrix.add(fila2);
+        matrix.add(fila1);
+        matrix.add(fila2);
+       
+        
+       tablero.setLista(matrix);
+       
+       
+        ArrayList<ArrayList<String>> matrix2=new ArrayList<ArrayList<String>>();
+        matrix2.add(fila1);
+        matrix2.add(fila2);
+        matrix2.add(fila1);
+        matrix2.add(fila2);
+        matrix2.add(fila1);
+        matrix2.add(fila2);
+        matrix2.add(fila1);
+        matrix2.add(fila2);
+        
+        tablero.setLista(matrix2);
+       // tablero.setLista(matrix);
+       //System.out.println(tablero.getMatrix().toString());
+      
+
+
+
+*/
--- src/JuegoDeDamas/Juego/Tablero.java
+++ src/JuegoDeDamas/Juego/Tablero.java
@@ -3,16 +3,34 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package JuegoDeDamas.Juego;
+
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Graphics;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-public class Tablero{
-    JFrame frame = new JFrame();
+import javax.swing.SwingUtilities;
+
+/**
+ *
+ * @author Asus
+ */
+public class Tablero extends JPanel{
+    
     ArrayList<ArrayList<String>> matrix=new ArrayList<ArrayList<String>>();
+    
+    private static final int DIM_WIDTH = 640;
+    private static final int DIM_HEIGHT = 640;
+    private static final int SQ_SIZE = 80;
+
+    boolean black = true;
+
     public Tablero(){
-        ArrayList<String>fila1=new ArrayList<String>();
+         ArrayList<String>fila1=new ArrayList<String>();
         fila1.add("0");fila1.add("1");fila1.add("0");fila1.add("1");fila1.add("0");fila1.add("1");fila1.add("0");fila1.add("1");
 
         ArrayList<String>fila2=new ArrayList<String>();
@@ -38,94 +56,71 @@ public class Tablero{
         matrix.add(fila2);
         matrix.add(filaCPN);
         matrix.add(filaCPN2);
-
-          
-          frame.setBounds(10, 10, 512, 512);
-          frame.setUndecorated(false);
-          JPanel pn=new JPanel(){
-              @Override
-              public void paint(Graphics g) {
-              boolean white=true;
-                   for(int y= 0;y<8;y++){
-                          for(int x= 0;x<8;x++){
-                          if(matrix.get(y).get(x).compareTo("1")==0){
-                              g.setColor(new Color(235,235, 208));
-                          }else{
-                              if(matrix.get(y).get(x).compareTo("0")==0){
-                                   g.setColor(new Color(119, 148, 85));
-                              }else{
-                                  if(matrix.get(y).get(x).compareTo("B")==0){
-                                      g.setColor(new Color(19, 18, 85));
-                                      
-                                  }
-                                  else{
-                                        if(matrix.get(y).get(x).compareTo("N")==0){
-                                          g.setColor(new Color(255, 18, 85));
-                                  }
-                                  }
-                              }
-                          }
-                          g.fillRect(x*64, y*64, 64, 64);
-                           white=!white;
-                      }
-                      white=!white;
-                  }
-
-              }
-
-          };
-          frame.add(pn);
-          frame.setDefaultCloseOperation(3);
-          frame.setVisible(true);
-        }
     
-    public ArrayList<ArrayList<String>> getMatrix(){
-        return matrix;
     }
     
-    public void setTablero(ArrayList<ArrayList<String>> matrix){
-        
-        
-         // JFrame frame = new JFrame();
-          frame.setBounds(10, 10, 512, 512);
-          frame.setUndecorated(false);
-          JPanel pn=new JPanel(){
-              @Override
-              public void paint(Graphics g) {
-              boolean white=true;
-                   for(int y= 0;y<8;y++){
-                          for(int x= 0;x<8;x++){
-                          if(matrix.get(y).get(x).compareTo("1")==0){
-                              g.setColor(new Color(235,235, 208));
-                              g.fillOval(x, y, 30, 30);
-                          }else{
-                              if(matrix.get(y).get(x).compareTo("0")==0){
-                                   g.setColor(new Color(119, 148, 85));
-                              }else{
-                                  if(matrix.get(y).get(x).compareTo("B")==0){
-                                      g.setColor(new Color(19, 18, 85));
-                                  }
-                                  else{
-                                        if(matrix.get(y).get(x).compareTo("N")==0){
-                                          g.setColor(new Color(255, 18, 85));
-                                          
-                                         
-                                  }
-                                  }
-                              }
-                          }
-                          g.fillRect(x*64, y*64, 64, 64);
-                           white=!white;
-                      }
-                      white=!white;
+    @Override
+    public void paint(Graphics g) {
+        boolean white=true;
+        for(int y= 0;y<matrix.size();y++){
+            for(int x= 0;x<matrix.get(y).size();x++){
+                if(matrix.get(y).get(x).compareTo("1")==0){
+                    g.setColor(new Color(235,235, 208));
+                }else{
+                if(matrix.get(y).get(x).compareTo("0")==0){
+                    g.setColor(new Color(119, 148, 85));
+                }else{
+                if(matrix.get(y).get(x).compareTo("B")==0){
+                    g.setColor(new Color(19, 18, 85));          
+                }else{
+                if(matrix.get(y).get(x).compareTo("N")==0){
+                    g.setColor(new Color(255, 18, 85));
+                    }
+                }
+                }
+                }
+                g.fillRect(x*64, y*64, 64, 64);
+                white=!white;
+                }
+                white=!white;
                   }
+    }
+    
+     public  void createAndShowGui() {
+        JFrame frame = new JFrame();
+        frame.add(new Tablero());
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationByPlatform(true);
+        frame.pack();
+        frame.setVisible(true);
 
-              }
-                
-          };
-          frame.add(pn);
-          frame.setDefaultCloseOperation(3);
-          frame.setVisible(true);
-          
+    }
+
+    public Dimension getPreferredSize() {
+        return new Dimension(DIM_WIDTH, DIM_HEIGHT);
+    }
+   /* 
+     public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGui();
+            }
+        });
+    }
+    */
+    
+    public ArrayList<ArrayList<String>> getMatrix(){
+        return matrix;    
+    }
+    
+    public void setLista(ArrayList<ArrayList<String>> matrix) {	
+        try {
+            Thread.sleep(1000);
+            this.matrix = matrix;         
+            repaint();
+        } catch (InterruptedException e) {
+            Logger.getLogger(Tablero.class.getName()).log(Level.SEVERE, null, e);
         }
+    }
+    
 }
